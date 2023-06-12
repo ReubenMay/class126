7768883c490636716d3d71a0d386479b7c7e129c
ReubenMay <95076512+ReubenMay@users.noreply.github.com>
2023-06-12 06:59:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+song="";
+
+function setup(){
+  canvas=  createCanvas(600,450);
+  canvas.center();
+  video=  createCapture(VIDEO);
+  video.hide()
+}
+function preload(){
+    song=loadSound("music.mp3");
+}
+function draw(){
+    image(video,0,0,600,450);
+}
+function play(){
+    song.play();
+}
